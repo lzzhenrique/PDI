f61693e50654eebfcad910eddbf14a14e30fec67
eng-luiz <luiz.silva@agilize.com.br>
2022-08-12 08:38:14 -0300

    ADD YAGNI

---
+++ src/YAGNI/YagniExample.php
@@ -0,0 +1,28 @@
+<?php
+
+class Pan 
+{
+    public function cookFood($food)
+    {
+        $food->setIsCooked(true);
+
+        return $food;
+    }
+
+    public function boilWater($water)
+    {
+        $water->setIsBoiled(true);
+
+        return $water;
+    }
+
+    // Definitivamente, uma panela pode ser usada para bater em alguem,
+    // mas está longe de ser necessario ter esse metodo ja implementado.
+    public function bonkPeople($people)
+    {
+        $bodyPartToHit = $people->getRandomBodyPart();
+        $bodyPartToHit->setIsHit(true);
+
+        $people->inflictDamage($bodyPartToHit);
+    }
+}
